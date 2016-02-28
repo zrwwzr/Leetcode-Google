8bc6c87ebb656dd38c432f886dd62eac2bfb353e
zengruiwang <zengruiwang@gmail.com>
2016-02-28 14:35:04 -0800

    Create 323. Number of Connected Components in an Undirected Graph.cpp

---
+++ 323. Number of Connected Components in an Undirected Graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countComponents(int n, vector<pair<int, int>>& edges) {
+        unordered_map<int, vector<int>> graph;
+        for(int i = 0; i < edges.size(); ++i){
+            graph[edges[i].first].push_back(edges[i].second);
+            graph[edges[i].second].push_back(edges[i].first);
+        }
+        queue<int> q;
+        vector<int> visited(n, false), neighbors;
+        int cnt = 0, cur;
+        for(int i = 0; i < n; ++i){
+            if(!visited[i]){
+                cnt++;
+                visited[i] = true;
+                q.push(i);
+                while(!q.empty()){
+                    cur = q.front(), q.pop();
+                    neighbors = graph[cur];
+                    for(int j = 0; j < neighbors.size(); ++j){
+                        if(!visited[neighbors[j]]){
+                            visited[neighbors[j]] = true;
+                            q.push(neighbors[j]);
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
