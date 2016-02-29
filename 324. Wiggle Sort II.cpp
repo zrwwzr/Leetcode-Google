d5d61c1bd665077fc0e2478be4c2aee88d4aaaa0
zengruiwang <zengruiwang@gmail.com>
2016-02-28 22:52:37 -0800

    Update 324. Wiggle Sort II.cpp

--- 324. Wiggle Sort II.cpp
+++ 324. Wiggle Sort II.cpp
@@ -7,7 +7,7 @@ public:
         sort(nums.begin(), nums.end());
         int i = (n + 1) >> 1, j = n;
         for(int k = 0; k < n; ++k)
-           temp[k] = k % 2 == 0 ? nums[--i] : nums[--j];
+           temp[k] = (k & 1 == 0) ? nums[--i] : nums[--j];
         nums.swap(temp);
     }
 };
