4a9ebbb3d310c1f4059ababc0e390d933b4c3265
zengruiwang <zengruiwang@gmail.com>
2016-02-28 22:11:03 -0800

    Refactored code

--- 324. Wiggle Sort II.cpp
+++ 324. Wiggle Sort II.cpp
@@ -3,15 +3,11 @@ public:
     void wiggleSort(vector<int>& nums) {
         if(nums.empty()) return;
         const size_t n = nums.size();
-        vector<int> temp;
+        vector<int> temp(n);
         sort(nums.begin(), nums.end());
-        int mid = n % 2 == 0 ? n / 2 : n / 2 + 1;
-        int i = mid - 1, j = n - 1, k = 0;
-        while(i >= 0 || j >= mid){
-            if(k % 2 == 0 && i >= 0) temp.push_back(nums[i--]);
-            else if(k % 2 != 0 && j >= mid) temp.push_back(nums[j--]);
-            k++;
-        }
+        int i = (n + 1) >> 1, j = n;
+        for(int k = 0; k < n; ++k)
+           temp[k] = k % 2 == 0 ? nums[--i] : nums[--j];
         nums.swap(temp);
     }
 };
