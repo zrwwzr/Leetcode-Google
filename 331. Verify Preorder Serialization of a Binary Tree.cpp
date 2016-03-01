1033cb87836973cc11a56f92f5758fe0d541171d
zengruiwang <zengruiwang@gmail.com>
2016-02-29 23:39:09 -0800

    Deleted vector<string> tokens

--- 331. Verify Preorder Serialization of a Binary Tree.cpp
+++ 331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -4,17 +4,15 @@ public:
     const string null = "#";
     bool isValidSerialization(string preorder) {
     	vector<string> stack;
-    	vector<string> tokens;
     	string token;
     	istringstream iss(preorder);
-    	while(getline(iss, token, deliminator))
-    	    tokens.push_back(token);
-    	for(int i = 0; i < tokens.size(); ++i){
-    		stack.push_back(tokens[i]);
+    	while(getline(iss, token, deliminator)){
+    		stack.push_back(token);
     		while(stack.size() > 2 && stack.back() == null && stack[stack.size() - 2] == null && stack[stack.size() - 3] != null){
     			stack.pop_back(), stack.pop_back(), stack.pop_back();
     			stack.push_back(null);
     		}
+    
     	}
     	return stack.size() == 1 && stack.back() == null;
     }
